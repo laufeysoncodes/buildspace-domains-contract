c9655fafe718073468265464656d7f98d982c733
laufeyson.eth <100027621+laufeysoncodes@users.noreply.github.com>
2022-03-25 23:11:36 +0800

    add: project contract

--- scripts/run.js
+++ scripts/run.js
@@ -1,16 +1,18 @@
 const main = async () => {
-    const [owner, randomPerson] = await hre.ethers.getSigners();
     const domainContractFactory = await hre.ethers.getContractFactory('Domains');
-    const domainContract = await domainContractFactory.deploy();
+    const domainContract = await domainContractFactory.deploy("code");
     await domainContract.deployed();
+  
     console.log("Contract deployed to:", domainContract.address);
-    console.log("Contract deployed by:", owner.address);
-    
-    const txn = await domainContract.register("doom");
+  
+    let txn = await domainContract.register("laufeyson",  {value: hre.ethers.utils.parseEther('0.1')});
     await txn.wait();
   
-    const domainOwner = await domainContract.getAddress("doom");
-    console.log("Owner of domain:", domainOwner);
+    const address = await domainContract.getAddress("laufeyson");
+    console.log("Owner of domain laufeyson:", address);
+  
+    const balance = await hre.ethers.provider.getBalance(domainContract.address);
+    console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
   }
   
   const runMain = async () => {
